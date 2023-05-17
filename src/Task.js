876c29d8435672f2b7e7d0bd07f90caa2464c5ce
Hilal <hilalyilmaz016@gmail.com>
2023-05-17 03:13:50 +0300

    tailwind css hatalı

--- src/Task.js
+++ src/Task.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { formatDistanceToNow , differenceInDays } from 'date-fns' ;
+import {tr } from 'date-fns/locale' ;
+
 
 const Task = ({ taskObj, onComplete }) => {
-
+  const result = formatDistanceToNow(new Date(taskObj.deadline) ,{
+    addSuffix : true,
+    locale : tr,
+  })
+  const days = differenceInDays(new Date(taskObj.deadline), new Date()) 
+ 
   return (
-    <div className="task">
+    <div className=" p-6 bg-white rounded-md leading-normal mt-4 shadow-md">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+      <div className=" text-xs py-1">son teslim: {" "}
+      <span className={days <= 3 ? "bg-[#ffd9d4]" : "bg-[#d2d5fd]"}> 
+        {result}
+      </span></div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
-          <span className="pill" key={p}>{p}</span>
+          <span className=" inline-block px-3 py-1 text-xs border-solid rounded-full" key={p}>{p}</span>
         ))}
       </div>
       {onComplete && <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>}
